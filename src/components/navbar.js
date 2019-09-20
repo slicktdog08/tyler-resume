b53295a7b2bfabfffb177526882c519582c43dad
slicktdog08 <tylerclay1@gmail.com>
2019-09-20 11:00:26 -0500

    Removed javascript based routing - Routing rewrite

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleRoot } from 'radium'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 
 class navbar extends React.Component{
     constructor(props){
@@ -79,13 +79,7 @@ class navbar extends React.Component{
       }
     }
 
-    navbarController = (event) =>{
-      this.setState({
-        page: event.target.id
-      }, ()=>{
-        this.props.history.push(`/${this.state.page}`)
-      })
-    }
+    
     mobileMenuToggle = () =>{
       var oldState = Object.assign({}, this.state)
       this.setState({
@@ -103,30 +97,30 @@ class navbar extends React.Component{
                   {/* Collect the nav links, forms, and other content for toggling */}
                   <div className={this.state.expanded ? 'navbar-collapse' : 'collapse navbar-collapse'} id="nav-tabis" style={AnimationStyle.styles.bounce_in_left}>
                     <ul className="isop-filter nav nav-pills">
-                      <li role="presentation" className={this.state.page === 'aboutMe' ? 'active': ''} >
-                        <a href="javascript:void(0)" id="aboutMe" role="tab" data-toggle="tab" onClick={this.navbarController}>
+                      <li role="presentation" className={this.props.location.pathname === '/aboutMe' || this.props.location.pathname === '/' ? 'active': ''} >
+                        <Link to='/aboutMe' role="tab" data-toggle="tab" >
                           <i className="icon-user" /> ABOUT ME
-                        </a>
+                        </Link>
                       </li>
-                      <li role="presentation" className={this.state.page === 'resume' ? 'active': ''}>
-                        <a href="javascript:void(0)" id="resume" role="tab" data-toggle="tab" onClick={this.navbarController}>
+                      <li role="presentation" className={this.props.location.pathname === '/resume' ? 'active': ''}>
+                        <Link to='/resume' role="tab" data-toggle="tab">
                           <i className="icon-book-open" />RESUME
-                        </a>
+                        </Link>
                       </li>
-                      <li role="presentation" className={this.state.page === 'portfolio' ? 'active': ''}>
-                        <a href="javascript:void(0)" id="portfolio" role="tab" data-toggle="tab" onClick={this.navbarController}>
+                      <li role="presentation" className={String(this.props.location.pathname).substring(0,10) === '/portfolio' ? 'active': ''}>
+                        <Link to='/portfolio' role="tab" data-toggle="tab">
                           <i className="icon-rocket" />PORTFOLIO
-                        </a>
+                        </Link>
                       </li>
-                      <li role="presentation" className={this.state.page === 'blog' ? 'active': ''}>
-                        <a href="javascript:void(0)" id="blog" role="tab" data-toggle="tab" onClick={this.navbarController}>
+                      <li role="presentation" className={String(this.props.location.pathname).substring(0,5) === '/blog'  ? 'active': ''}>
+                        <Link to='/blog' role="tab" data-toggle="tab">
                           <i className="icon-note" />BLOG
-                        </a>
+                        </Link>
                       </li>
-                      <li role="presentation" className={this.state.page === 'contact' ? 'active': ''}>
-                        <a href="javascript:void(0)" id="contact" role="tab" data-toggle="tab" onClick={this.navbarController}>
+                      <li role="presentation" className={this.props.location.pathname === '/contact' ? 'active': ''}>
+                        <Link to='/contact' role="tab" data-toggle="tab">
                           <i className="icon-pencil" />CONTACT ME
-                        </a>
+                        </Link>
                       </li>
                     </ul>
                   </div>
